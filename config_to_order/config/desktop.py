4f4b18f9272f3f40a99e0445586c1f0825b8b183
szfisher <yuxinyong@163.com>
2020-03-09 15:33:37 +0800

    Add files via upload

---
+++ config_to_order/config/desktop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Config To Order",
+			"color": "blue",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Config To Order")
+		}
+	]
